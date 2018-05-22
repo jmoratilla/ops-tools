4553e9bdeb727b3bf72334a71b5bfb4fabb2d282
Jorge Moratilla <jorge.moratilla@kelisto.es>
2018-05-22 23:54:30 +0200

    Now csv-splitter works with defaults

--- csv-splitter/csv-splitter.rb
+++ csv-splitter/csv-splitter.rb
@@ -1,12 +1,13 @@
 # coding: utf-8
-require 'optparse'
 # Author: Jorge Moratilla Porras
 # Description: To split a large CSV file into smaller files
+
+require 'optparse'
+require 'pp'
 
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: csv-splitter.rb <input_file> [options]"
-
   opts.on("-nnumber_of_records", "--name=number_of_records", "Number of records per file") do |n|
     options['number_of_records'] = n
   end
@@ -15,10 +16,12 @@ OptionParser.new do |opts|
   end
 end.parse!
 
+pp options
+
 # args
 input_file = ARGV.shift
-output_file = options['output_file'] || "output"
-number_of_records = options['number_of_records'].to_i || 10000
+output_file = options['output_file'] ? options['output_file'] : 'output'
+number_of_records = options['number_of_records'] ? options['number_of_records'].to_i : 10000
 
 output_suffix = 0
 headers = nil
