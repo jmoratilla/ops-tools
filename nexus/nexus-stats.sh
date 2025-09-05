a7fd2ae5761267dc9c19c1dd2d5866cf2774adf0
Jorge Moratilla <jorge.moratilla@mylighthouse.com>
2025-09-05 09:22:42 +0200

    fix: improve security

--- nexus/nexus-stats.sh
+++ nexus/nexus-stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="http://repo.services.aws.bqreaders.local/nexus/internal/metrics"
-USER="admin"
-PASS="c0rch0l1s!"
+USER="xxx"
+PASS="xxx"
 
 watch "curl -s -X GET --user \"${USER}:${PASS}\" ${URL} | python -m json.tool"
